b18e53b5f632323f01a31b88b64e8ca5a8718aa5
LucasMostafa <mostafalucas@gmail.com>
2026-02-05 16:13:16 -0300

    Section studio

--- src/app/features/home/pages/home-view/home-view.ts
+++ src/app/features/home/pages/home-view/home-view.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { BentoGrid } from '../../components/bento-grid/bento-grid';
 import { Navbar } from '../../../../core/components/navbar/navbar';
 import { WhatWeDo } from '../../components/what-we-do/what-we-do';
+import { Studio } from '../../components/studio/studio';
 
 @Component({
   selector: 'app-home-view',
-  imports: [BentoGrid, Navbar, WhatWeDo],
+  imports: [BentoGrid, Navbar, WhatWeDo, Studio],
   templateUrl: './home-view.html',
   styleUrl: './home-view.scss',
 })
