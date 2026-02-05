d4b4d912edba6e2cbaef8d1573ef64417ee848bd
LucasMostafa <mostafalucas@gmail.com>
2026-02-05 14:29:00 -0300

    Tu mensaje describiendo el cambio

--- src/app/features/home/components/what-we-do/what-we-do.ts
+++ src/app/features/home/components/what-we-do/what-we-do.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-what-we-do',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './what-we-do.html',
-  styleUrl: './what-we-do.scss',
+  styleUrl: './what-we-do.scss'
 })
 export class WhatWeDo {
 
+  // Función para reproducir el video al entrar
+  playVideo(videoElement: HTMLVideoElement) {
+    videoElement.currentTime = 0;
+    // Forzar el mute ayuda a que el navegador permita el autoplay
+    videoElement.muted = true; 
+    
+    videoElement.play().catch(error => {
+      console.log('Video play prevented (normal si no hubo interacción previa):', error);
+    });
+  }
+
+  // Función para detener el video al salir
+  stopVideo(videoElement: HTMLVideoElement) {
+    videoElement.pause();
+  }
 }
