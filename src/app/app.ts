22012d72b6ae5d5587ecd0e2f65fa1a266332d5b
LucasMostafa <mostafalucas@gmail.com>
2026-02-07 13:34:46 -0300

    Componente contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './core/components/navbar/navbar';
+//import { Navbar } from './core/components/navbar/navbar';
 import { Footer } from './core/components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Footer], // <--- AGREGALOS ACÁ
+  imports: [RouterOutlet, Footer], // <--- AGREGALOS ACÁ
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/features/home/pages/home-view/home-view.ts
+++ src/app/features/home/pages/home-view/home-view.ts
@@ -3,10 +3,10 @@ import { BentoGrid } from '../../components/bento-grid/bento-grid';
 import { Navbar } from '../../../../core/components/navbar/navbar';
 import { WhatWeDo } from '../../components/what-we-do/what-we-do';
 import { Studio } from '../../components/studio/studio';
-
+import { Contact } from '../../components/contact/contact';
 @Component({
   selector: 'app-home-view',
-  imports: [BentoGrid, Navbar, WhatWeDo, Studio],
+  imports: [BentoGrid, Navbar, WhatWeDo, Studio, Contact],
   templateUrl: './home-view.html',
   styleUrl: './home-view.scss',
 })
