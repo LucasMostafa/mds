6127d37227fdce28ce3cc3f0b2a7fae780b8d3a8
LucasMostafa <mostafalucas@gmail.com>
2026-02-11 23:45:53 -0300

    Footer scrolleo

--- src/app/core/components/footer/footer.ts
+++ src/app/core/components/footer/footer.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class Footer {
-  currentYear = new Date().getFullYear();
   
+  currentYear = new Date().getFullYear();
+
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    
+    if (element) {
+      // OFFSET AGRESIVO: 150px
+      // Esto fuerza al scroll a detenerse mucho antes de llegar al título
+      // para compensar tu Navbar flotante grande.
+      const offset = 0; 
+      
+      const bodyRect = document.body.getBoundingClientRect().top;
+      const elementRect = element.getBoundingClientRect().top;
+      const elementPosition = elementRect - bodyRect;
+      const offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    } else {
+      console.warn('No se encontró la sección:', sectionId);
+    }
+  }
 }
